24090755620e86b18f68e26e446db4c8286b9e48
cid.immacula <cid.immacula@gmail.com>
2019-02-02 00:59:35 +0800

    Config
    -Removed Magma 2 tanking from H_Tactics into H_GroupTactics
    -Disabled Magma 2 tanking

--- USER_AI/H_GroupTactics.lua
+++ USER_AI/H_GroupTactics.lua
@@ -7,3 +7,4 @@
 --MyGroupTact[x][4] = tactic
 MyGroupTact={}
 MyGroupTact[1]={{"Plant Protocol"},{1078,1078,1080,1081,1082,1083,1084,1085},{"Red Plant", "Blue Plant", "Green Plant", "Yellow Plant", "White Plant", "Shining Plant", "Black Mushroom", "Red Mushroom"},{TACT_IGNORE,SKILL_NEVER,KITE_NEVER,CAST_REACT,PUSH_NEVER,DEBUFF_NEVER,CLASS_BOTH,RESCUE_NEVER,-1,SNIPE_OK,KS_NEVER,0,CHASE_NORMAL}}
+--MyGroupTact[2]={{"Magma 2 Tanking"},{1367,1379,1382,1384,1385,1387},{"Blazer","Nightmare Terror","Diabolic","Deleter S","Deleter G","Gig"},{TACT_TANK,SKILL_NEVER,KITE_REACT,CAST_REACT,PUSH_NEVER,DEBUFF_NEVER,CLASS_BOTH,RESCUE_NEVER,-1,SNIPE_OK,KS_POLITE,1,CHASE_NEVER}}
